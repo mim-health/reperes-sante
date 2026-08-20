5d7ed50c278fd97b6c0d7c368324dc20f2278c27
mim-health <dr.beddok@gmail.com>
2026-08-20 18:47:09 +0200

    Add precise source registry for 26 audited health questions

---
+++ source-registry.js
@@ -0,0 +1,82 @@
+window.santeJusteSourceRegistry = {
+'fievre-enfant':[
+{org:'Haute Autorité de Santé',title:'Prise en charge de la fièvre chez l’enfant',year:'2016, page actualisée',url:'https://www.has-sante.fr/jcms/c_2674284/fr/prise-en-charge-de-la-fievre-chez-l-enfant'},
+{org:'Assurance Maladie',title:'Votre enfant a de la fièvre : que faire et quand consulter ?',year:'mise à jour en ligne',url:'https://www.ameli.fr/assure/sante/themes/fievre-enfant/bons-reflexes-cas-faut-consulter'},
+{org:'Société Française de Pédiatrie',title:'Fiche mémo – Prise en charge de la fièvre chez l’enfant',year:'référentiel pédiatrique',url:'https://www.sfpediatrie.com/sites/www.sfpediatrie.com/files/medias/documents/fiche_memo_-_prise_en_charge_de_la_fievre_chez_lenfant.pdf'}],
+'diarrhee-enfant':[
+{org:'GFHGNP',title:'Diarrhée aiguë du nourrisson et de l’enfant – Recommandations d’experts 2024',year:'2024',url:'https://www.gfhgnp.org/recommandations-et-documents/diarrhee-aigue-du-nourrisson-et-de-lenfant/'},
+{org:'Assurance Maladie',title:'Diarrhée et gastro-entérite de l’enfant : que faire et quand consulter ?',year:'2025',url:'https://www.ameli.fr/assure/sante/themes/gastro-enterite-enfant/que-faire-quand-consulter'}],
+'bebe-tete-moins':[
+{org:'1000 premiers jours',title:'Guide de l’allaitement maternel',year:'guide national',url:'https://www.1000-premiers-jours.fr/sites/default/files/2021-09/Guide%20de%20l%27allaitement%20maternel.pdf'},
+{org:'Assurance Maladie',title:'Allaitement maternel : conseils pratiques',year:'page d’information',url:'https://www.ameli.fr/assure/sante/themes/allaitement'}],
+'toux-enfant':[
+{org:'Assurance Maladie',title:'Quand consulter et comment calmer la toux ?',year:'mise à jour en ligne',url:'https://www.ameli.fr/assure/sante/themes/toux/calmer-toux-cas-faut-consulter'},
+{org:'Société Française de Pédiatrie / CNPU',title:'Attendus d’apprentissage – situation clinique « toux »',year:'référentiel pédiatrique',url:'https://www.sfpediatrie.com/sites/www.sfpediatrie.com/files/medias/documents/attendus_apprentissage_cnpu_journee_janvier_2021_finale.pdf'},
+{org:'HAS / Société Française de Pédiatrie',title:'Prise en charge du premier épisode de bronchiolite aiguë chez le nourrisson de moins de 12 mois',year:'2019',url:'https://www.sfpediatrie.com/sites/www.sfpediatrie.com/files/medias/documents/hascnpp_bronchiolite_texte_recommandations_2019.pdf'}],
+'vomissements-enfant':[
+{org:'GFHGNP',title:'Diarrhée aiguë du nourrisson et de l’enfant – Recommandations d’experts 2024',year:'2024',url:'https://www.gfhgnp.org/recommandations-et-documents/diarrhee-aigue-du-nourrisson-et-de-lenfant/'},
+{org:'Assurance Maladie',title:'Diarrhée et gastro-entérite de l’enfant : que faire et quand consulter ?',year:'2025',url:'https://www.ameli.fr/assure/sante/themes/gastro-enterite-enfant/que-faire-quand-consulter'}],
+'rash-enfant':[
+{org:'Assurance Maladie',title:'Éruptions cutanées : informations et situations nécessitant un avis',year:'page d’information',url:'https://www.ameli.fr/assure/sante/themes/eruption-cutanee'},
+{org:'Ministère de la Santé',title:'Carnet de santé de l’enfant en vigueur au 1er janvier 2025',year:'2025',url:'https://sante.gouv.fr/IMG/pdf/carnet_de_sante-_2025.pdf'}],
+'sommeil-bebe':[
+{org:'Ministère de la Santé',title:'Carnet de santé de l’enfant – conseils pour bien dormir',year:'2025',url:'https://sante.gouv.fr/IMG/pdf/carnet_de_sante-_2025.pdf'},
+{org:'Société Française de Pédiatrie',title:'Prévention contre la mort inattendue du nourrisson',year:'2023',url:'https://www.sfpediatrie.com/actualites/semaine-annuelle-prevention-contre-mort-inattendue-du-nourrisson'}],
+'ecrans-petit':[
+{org:'Ministère de la Santé',title:'Enfants et écrans : des risques sanitaires réels, un accompagnement nécessaire',year:'2025',url:'https://sante.gouv.fr/prevention-en-sante/sante-des-populations/enfants/exposition-aux-ecrans/article/enfants-et-ecrans-des-risques-sanitaires-reels-un-accompagnement-necessaire'},
+{org:'Ministère de la Santé / HCSP',title:'Carnet de santé de l’enfant – repères sur les écrans',year:'2025',url:'https://sante.gouv.fr/IMG/pdf/carnet_de_sante-_2025.pdf'}],
+'paracetamol':[
+{org:'Haute Autorité de Santé',title:'Prise en charge de la fièvre chez l’enfant',year:'2016, toujours référencé',url:'https://www.has-sante.fr/jcms/c_2674284/fr/prise-en-charge-de-la-fievre-chez-l-enfant'},
+{org:'Société Française de Pédiatrie',title:'Fiche mémo – Prise en charge de la fièvre chez l’enfant',year:'référentiel pédiatrique',url:'https://www.sfpediatrie.com/sites/www.sfpediatrie.com/files/medias/documents/fiche_memo_-_prise_en_charge_de_la_fievre_chez_lenfant.pdf'},
+{org:'HAS',title:'RCP pédiatrique Efferalganmed – posologie selon le poids',year:'2026',url:'https://www.has-sante.fr/upload/docs/evamed/CT-20709_EFFEERALGANMED_PEDIATRIQUE_PIS_RCP_AvisDef_CT20709.pdf'}],
+'ibuprofene-enfant':[
+{org:'Haute Autorité de Santé',title:'Prise en charge de la fièvre chez l’enfant',year:'référentiel',url:'https://www.has-sante.fr/jcms/c_2674284/fr/prise-en-charge-de-la-fievre-chez-l-enfant'},
+{org:'HAS / SPILF / SFP-GPIP',title:'Rhinopharyngite aiguë de l’enfant – traitement symptomatique et AINS',year:'actualisation 2025',url:'https://www.has-sante.fr/jcms/c_2722754/fr/choix-et-durees-d-antibiotherapies-rhinopharyngite-aigue-enfant'},
+{org:'ANSM',title:'Information sur les antipyrétiques chez l’enfant : précautions avec les AINS',year:'référence de sécurité',url:'https://archive.ansm.sante.fr/var/ansm_site/storage/original/application/b439d55ee299ae83db7bd9008c1eb267.pdf'}],
+'antibiotiques':[
+{org:'Santé publique France',title:'Résistance aux antibiotiques',year:'mise à jour 2025-2026',url:'https://www.santepubliquefrance.fr/resistance-aux-antibiotiques'},
+{org:'Santé publique France',title:'Les antibiotiques, ça ne marche pas contre la grippe',year:'2022, mise à jour 2026',url:'https://www.santepubliquefrance.fr/resistance-aux-antibiotiques/affiche/thierry-peut-vous-le-confirmer-les-antibiotiques-ca-ne-marche-pas-contre-sa-grippe-affiche-40x60cm'},
+{org:'HAS / SPILF',title:'Rhinopharyngite aiguë et angine aiguë de l’enfant – antibiothérapie',year:'référentiel français',url:'https://www.has-sante.fr/jcms/c_2722754/fr/choix-et-durees-d-antibiotherapies-rhinopharyngite-aigue-enfant'}],
+'hta':[
+{org:'Société Française d’Hypertension Artérielle',title:'Relevé d’automesure tensionnelle – règle des 3',year:'2025',url:'https://www.sfhta.eu/wp-content/uploads/2025/06/releve_automesure_tensionnelle_cnam-1.pdf'},
+{org:'Haute Autorité de Santé',title:'Prise en charge de l’hypertension artérielle de l’adulte',year:'fiche mémo',url:'https://www.has-sante.fr/upload/docs/application/pdf/2016-10/fiche_memo_rapport_elaboration_hta_mel.pdf'}],
+'pas-jour':[
+{org:'The Lancet Public Health',title:'Daily steps and health outcomes in adults: systematic review and dose-response meta-analysis',year:'2025',url:'https://doi.org/10.1016/S2468-2667(25)00164-1'}],
+'fibres':[
+{org:'Anses',title:'Références nutritionnelles et repères alimentaires – fibres chez l’adulte',year:'référence nationale',url:'https://www.anses.fr/system/files/NUT2017SA0143.pdf'}],
+'boissons-sucrees':[
+{org:'Haut Conseil de la santé publique',title:'Révision des repères alimentaires pour les enfants de 0-36 mois et 3-17 ans',year:'2020',url:'https://www.sfpediatrie.com/sites/www.sfpediatrie.com/files/medias/documents/hcspa20200630_rvisidesreprealimepourlesenfan.pdf'},
+{org:'Anses',title:'Nutrition des enfants : repères alimentaires',year:'référence nationale',url:'https://www.anses.fr/fr/content/nutrition-des-enfants-des-personnes-agees-et-des-femmes-enceintes-ou-allaitantes-lanses'}],
+'memoire':[
+{org:'SFGG / FCM / sociétés partenaires',title:'Recommandations pour le diagnostic de la maladie d’Alzheimer',year:'2026',url:'https://sfgg.org/rapports-etudes/recommandations-pour-le-diagnostic-de-la-maladie-dalzheimer/'},
+{org:'Assurance Maladie',title:'Maladie d’Alzheimer : symptômes et diagnostic',year:'page d’information',url:'https://www.ameli.fr/assure/sante/themes/maladie-alzheimer'}],
+'chutes':[
+{org:'Haute Autorité de Santé',title:'Personnes âgées à risque de chute – activité physique et prévention',year:'2024',url:'https://www.has-sante.fr/upload/docs/application/pdf/2024-04/synthese_aps_personnes_agees_a_risque_de_chute.pdf'},
+{org:'Société Française de Gériatrie et Gérontologie',title:'Prévention des chutes accidentelles chez la personne âgée',year:'référentiel',url:'https://sfgg.org/recommandations/prevention-des-chutes-accidentelles-chez-la-personne-agee/'}],
+'tique':[
+{org:'Haute Autorité de Santé',title:'Borréliose de Lyme et autres maladies vectorielles à tiques',year:'2025',url:'https://www.has-sante.fr/upload/docs/application/pdf/2025-02/borreliose_de_lyme_et_autres_maladies_vectorielles_a_tiques_mvt_-_argumentaire.pdf'},
+{org:'Assurance Maladie',title:'Maladie de Lyme : que faire après une piqûre de tique ?',year:'page d’information',url:'https://www.ameli.fr/assure/sante/themes/maladie-lyme'}],
+'brulure':[
+{org:'Assurance Maladie',title:'Brûlures de la peau : que faire et quand consulter ?',year:'page d’urgence',url:'https://www.ameli.fr/assure/sante/urgence/accidents-domestiques/brulures-peau'}],
+'douleur-abdominale':[
+{org:'Assurance Maladie',title:'Mal de ventre : que faire et quand consulter ?',year:'2025',url:'https://www.ameli.fr/assure/sante/themes/mal-ventre/que-faire-cas-faut-consulter'},
+{org:'SNFGE',title:'Référentiels et informations de gastro-entérologie',year:'société savante',url:'https://www.snfge.org/'}],
+'maux-tete':[
+{org:'Assurance Maladie',title:'Mal de tête : comment être soulagé(e) et quand consulter ?',year:'2026',url:'https://www.ameli.fr/assure/sante/themes/mal-tete/calmer-cas-faut-consulter'},
+{org:'Société Française d’Étude des Migraines et Céphalées',title:'Référentiels céphalées et migraines',year:'société savante',url:'https://sfemc.fr/'}],
+'etat-general-nourrisson':[
+{org:'Haute Autorité de Santé',title:'Prise en charge de la fièvre chez l’enfant – signes de gravité chez le nourrisson',year:'référentiel',url:'https://www.has-sante.fr/jcms/c_2674284/fr/prise-en-charge-de-la-fievre-chez-l-enfant'},
+{org:'Ministère de la Santé',title:'Carnet de santé de l’enfant',year:'2025',url:'https://sante.gouv.fr/IMG/pdf/carnet_de_sante-_2025.pdf'}],
+'difficulte-respiratoire-enfant':[
+{org:'Assurance Maladie',title:'Quand consulter et comment calmer la toux ? – signes respiratoires chez l’enfant',year:'mise à jour en ligne',url:'https://www.ameli.fr/assure/sante/themes/toux/calmer-toux-cas-faut-consulter'},
+{org:'HAS / Société Française de Pédiatrie',title:'Prise en charge du premier épisode de bronchiolite aiguë chez le nourrisson',year:'2019',url:'https://www.sfpediatrie.com/sites/www.sfpediatrie.com/files/medias/documents/hascnpp_bronchiolite_texte_recommandations_2019.pdf'}],
+'deshydratation-enfant':[
+{org:'GFHGNP',title:'Diarrhée aiguë du nourrisson et de l’enfant – Recommandations d’experts 2024',year:'2024',url:'https://www.gfhgnp.org/recommandations-et-documents/diarrhee-aigue-du-nourrisson-et-de-lenfant/'},
+{org:'Assurance Maladie',title:'Déshydratation : signes et réhydratation orale chez l’enfant',year:'page d’urgence',url:'https://www.ameli.fr/assure/sante/urgence/pathologies/deshydratation'}],
+'rhume-nez-bouche-enfant':[
+{org:'Haute Autorité de Santé / SPILF / SFP-GPIP',title:'Rhinopharyngite aiguë de l’enfant',year:'actualisation 2025',url:'https://www.has-sante.fr/jcms/c_2722754/fr/choix-et-durees-d-antibiotherapies-rhinopharyngite-aigue-enfant'},
+{org:'Assurance Maladie',title:'Rhinopharyngite de l’enfant : mesures utiles et surveillance',year:'page d’information',url:'https://www.ameli.fr/assure/sante/themes/rhinopharyngite-enfant'}],
+'douleur-oreille-enfant':[
+{org:'Haute Autorité de Santé / SPILF / GPIP',title:'Otite moyenne aiguë purulente de l’enfant de plus de 3 mois',year:'actualisation 2025',url:'https://www.has-sante.fr/jcms/c_2722749/fr/otite-moyenne-aigue-purulente-de-l-enfant-de-plus-de-3-mois'},
+{org:'Assurance Maladie',title:'Otite moyenne aiguë de l’enfant',year:'page d’information',url:'https://www.ameli.fr/assure/sante/themes/otite-moyenne-aigue'}]
+};
