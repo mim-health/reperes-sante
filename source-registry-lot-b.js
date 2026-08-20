1d57ab9f5bd04fbb3f4b95b02f73d25cddee4971
mim-health <dr.beddok@gmail.com>
2026-08-20 18:57:05 +0200

    Add precise sources for pediatric lot B

---
+++ source-registry-lot-b.js
@@ -0,0 +1,19 @@
+Object.assign(window.santeJusteSourceRegistry = window.santeJusteSourceRegistry || {}, {
+'constipation-enfant':[
+{org:'Assurance Maladie',title:'Constipation de l’enfant',year:'mise à jour 14 mars 2025',url:'https://www.ameli.fr/assure/sante/themes/constipation-enfant'},
+{org:'GFHGNP',title:'Constipation du nourrisson et de l’enfant – synthèse des JFHOD',year:'2024',url:'https://www.gfhgnp.org/wp-content/uploads/2024/05/Resumes-des-JFHOD-JLeruste.pdf'}],
+'regurgitations-nourrisson':[
+{org:'Haute Autorité de Santé',title:'Reflux gastro-œsophagien chez l’enfant de moins d’un an : définitions, prise en charge et pertinence des traitements pharmacologiques',year:'février 2024',url:'https://www.has-sante.fr/upload/docs/application/pdf/2024-07/fiche_-_reflux_gastro-oesophagien_chez_lenfant_de_moins_dun_an.pdf'},
+{org:'GFHGNP',title:'Reflux gastro-oesophagien du nourrisson : diagnostic et traitement',year:'référentiel de gastro-pédiatrie',url:'https://www.gfhgnp.org/recommandations-et-documents/reflux-gastrooesophagien-du-nourrisson/'},
+{org:'Assurance Maladie',title:'Régurgitations du nourrisson : que faire ?',year:'24 mars 2026',url:'https://www.ameli.fr/assure/sante/themes/rgo-nourrisson/que-faire-quand-consulter'}],
+'mal-gorge-enfant':[
+{org:'Haute Autorité de Santé / SPILF / SFP-GPIP',title:'Choix et durées d’antibiothérapies : angine aiguë de l’enfant',year:'mise à jour mai 2025',url:'https://www.has-sante.fr/upload/docs/application/pdf/2016-11/v1-fm_rhino-angine_enfant_cd-171116.pdf'},
+{org:'Assurance Maladie',title:'Mal de gorge : que faire et quand consulter ?',year:'23 avril 2025',url:'https://www.ameli.fr/assure/sante/themes/mal-gorge/que-faire-consultation-medicale'}],
+'oeil-rouge-enfant':[
+{org:'Société Française d’Ophtalmologie / AFSOP',title:'Conduite à tenir devant une conjonctivite de l’enfant',year:'mai 2017',url:'https://www.sfo-online.fr/sites/www.sfo-online.fr/files/medias/documents/conduite_atenir_devant_une_conjonctivite_de_l_enfant_afsop_mai2017.pdf'},
+{org:'Assurance Maladie',title:'Conjonctivite : que faire et quand consulter ?',year:'6 mai 2025',url:'https://www.ameli.fr/assure/sante/themes/conjonctivite/que-faire-quand-consulter'},
+{org:'Assurance Maladie',title:'Œil rouge : que faire et quand consulter ?',year:'2026',url:'https://www.ameli.fr/assure/sante/themes/oeil-rouge/que-faire-quand-consulter'}],
+'saignement-nez-enfant':[
+{org:'Assurance Maladie',title:'Saignement de nez ou épistaxis',year:'19 septembre 2025',url:'https://www.ameli.fr/assure/sante/urgence/pathologies/epistaxis-saignement-de-nez'},
+{org:'Société Française d’ORL et de Chirurgie Cervico-Faciale',title:'Mon enfant saigne du nez – fiche SOS ORL',year:'fiche patient',url:'https://www.orlfrance.org/urgences-orl/enfant-saigne-du-nez/'}]
+});
