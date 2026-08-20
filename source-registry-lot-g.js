faa059feada487124e37c06830319570611f4069
mim-health <dr.beddok@gmail.com>
2026-08-20 20:05:43 +0200

    Add source registry for pediatric lot G

---
+++ source-registry-lot-g.js
@@ -0,0 +1,18 @@
+window.santeJusteSourceRegistry = {...(window.santeJusteSourceRegistry || {}),
+'convulsion-febrile-enfant':[
+{org:'Assurance Maladie',title:'Convulsions fébriles de l’enfant',year:'20 mars 2025',url:'https://www.ameli.fr/assure/sante/urgence/bebe-enfant/convulsions-febriles-enfant'},
+{org:'Collège national des pédiatres universitaires',title:'Convulsions, crises épileptiques, épilepsie – référentiel de pédiatrie',year:'référence citée par l’Assurance Maladie',url:'https://www.ameli.fr/assure/sante/urgence/bebe-enfant/convulsions-febriles-enfant'}],
+'boiterie-enfant':[
+{org:'Assurance Maladie',title:'Mon enfant boite : que faire ?',year:'18 novembre 2025',url:'https://www.ameli.fr/assure/sante/themes/boiterie-enfant-adolescent/mon-enfant-boite-que-faire'},
+{org:'Société Française de Pédiatrie / GFRUP / SOFREMIP',title:'Boiterie de l’enfant – Pas à Pas en Pédiatrie',year:'référentiel validé par sociétés savantes',url:'https://pap-pediatrie.fr/pap-detail/bscmtgl'}],
+'diversification-alimentaire':[
+{org:'1000 premiers jours / Ministère de la Santé',title:'L’alimentation de 4 à 6 mois : le début de la diversification',year:'23 octobre 2025',url:'https://www.1000-premiers-jours.fr/fr/lalimentation-de-4-6-mois-le-debut-de-la-diversification'},
+{org:'Assurance Maladie',title:'La diversification alimentaire',year:'12 novembre 2024',url:'https://www.ameli.fr/assure/sante/themes/alimentation/alimentation-0-3-ans/debut-diversification-alimentaire'},
+{org:'Haut Conseil de la santé publique / Société Française de Pédiatrie',title:'Révision des repères alimentaires pour les enfants de 0-36 mois et 3-17 ans',year:'30 juin 2020, toujours utilisé dans les repères français actuels',url:'https://www.sfpediatrie.com/sites/www.sfpediatrie.com/files/medias/documents/hcspa20200630_rvisidesreprealimepourlesenfan.pdf'}],
+'ingestion-produit-toxique-enfant':[
+{org:'Assurance Maladie',title:'Intoxication par voie orale (par ingestion)',year:'11 septembre 2025',url:'https://www.ameli.fr/assure/sante/urgence/accidents-domestiques/intoxication-voie-orale-ingestion'},
+{org:'Assurance Maladie',title:'Mon enfant a avalé une pile bouton : agir vite',year:'20 novembre 2025',url:'https://www.ameli.fr/assure/sante/urgence/corps-etrangers/mon-enfant-avale-une-pile-bouton-agir-vite'},
+{org:'Carnet de santé de l’enfant',title:'Prévention des accidents domestiques et conduite en cas d’ingestion',year:'édition 2025',url:'https://www.ameli.fr/sites/default/files/Documents/carnet-sante-2025-specimen.pdf'}],
+'poux-enfant':[
+{org:'Société Française de Dermatologie – Dermato-info',title:'Poux / pédiculose : prise en charge pratique',year:'mise à jour 2026',url:'https://dermato-info.fr/sites/default/files/files/2026-03/LR%20Poux.pdf'}]
+};
