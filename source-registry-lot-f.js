bc1bdcfc92aee24947415c2534426b0421b8cdb5
mim-health <dr.beddok@gmail.com>
2026-08-20 19:34:24 +0200

    Add source registry for audited lot F

---
+++ source-registry-lot-f.js
@@ -0,0 +1,16 @@
+window.santeJusteSourceRegistry = {...(window.santeJusteSourceRegistry || {}),
+'constipation-adulte':[
+{org:'Assurance Maladie',title:'Constipation de l’adulte : que faire et quand consulter ?',year:'7 mars 2025',url:'https://www.ameli.fr/assure/sante/themes/constipation-adulte/que-faire-quand-consulter'},
+{org:'SNFGE / SNFCP / GFNG',title:'Recommandations pour la prise en charge de la constipation',year:'référentiel français en vigueur retrouvé',url:'https://www.snfge.org/recherche-documents?f%5B0%5D=categorie_string%3A54&f%5B1%5D=thematique_string%3A24'}],
+'vertiges-adulte':[
+{org:'Assurance Maladie',title:'Que faire en cas de vertiges ?',year:'29 septembre 2025',url:'https://www.ameli.fr/assure/sante/themes/vertiges/bons-reflexes-que-faire'},
+{org:'Haute Autorité de Santé / Collège de masso-kinésithérapie',title:'Vertiges positionnels paroxystiques bénins : manœuvres diagnostiques et thérapeutiques',year:'référence HAS toujours citée par Ameli en 2025',url:'https://www.has-sante.fr/jcms/c_2819893/fr/vertiges-positionnels-paroxystiques-benins-manoeuvres-diagnostiques-et-therapeutiques'}],
+'acouphenes-adulte':[
+{org:'Haute Autorité de Santé',title:'Acouphènes invalidants chez l’adulte : diagnostic et prise en charge',year:'16 juillet 2026',url:'https://www.has-sante.fr/jcms/p_4196187/fr/acouphenes-invalidants-des-cles-pour-ameliorer-le-diagnostic-et-la-prise-en-charge-des-adultes'},
+{org:'Haute Autorité de Santé',title:'Argumentaire — Acouphènes invalidants chez l’adulte',year:'juin 2026',url:'https://www.has-sante.fr/upload/docs/application/pdf/2026-07/dir6/acouphenes_argumentaire.pdf'}],
+'jambes-sans-repos':[
+{org:'Assurance Maladie',title:'Impatiences : que faire et quand consulter ?',year:'17 avril 2026',url:'https://www.ameli.fr/assure/sante/themes/syndrome-jambes-sans-repos-impatiences/bons-reflexes-cas-faut-consulter'}],
+'anxiete-adulte':[
+{org:'Assurance Maladie',title:'Angoisse, anxiété et troubles anxieux graves de l’adulte',year:'7 janvier 2026',url:'https://www.ameli.fr/assure/sante/themes/sante-mentale-de-l-adulte/angoisse-anxiete-et-troubles-anxieux-graves-de-l-adulte'},
+{org:'Assurance Maladie',title:'Traitement des troubles anxieux de l’adulte',year:'23 avril 2025',url:'https://www.ameli.fr/assure/sante/themes/troubles-anxieux-anxiete/traitement'}]
+};
