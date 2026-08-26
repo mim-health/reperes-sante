e3f3df372f99e28fb7244242c8b9d37e2dc26193
mim-health <dr.beddok@gmail.com>
2026-08-26 07:42:20 +0200

    content: register Alzheimer treatment sheet in corpus

--- corpus-manifest.js
+++ corpus-manifest.js
@@ -44,5 +44,6 @@ window.MACA_CORPUS_MANIFEST = Object.freeze([
   'backlog-audited-varices-2026-08-25.js?v=20260825-1',
   'backlog-audited-senior-cognition-ldl-2026-08-25.js?v=20260825-1',
   'backlog-audited-migraine-2026-08-25.js?v=20260825-1',
-  'backlog-audited-alopecie-pelade-2026-08-26.js?v=20260826-1'
+  'backlog-audited-alopecie-pelade-2026-08-26.js?v=20260826-1',
+  'backlog-audited-alzheimer-traitements-2026-08-26.js?v=20260826-1'
 ]);
