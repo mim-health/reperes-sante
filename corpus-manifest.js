b4eb320a97b07edc07b2a8158554ad50e2fedec7
mim-health <dr.beddok@gmail.com>
2026-08-26 17:42:12 +0200

    MACA corpus manifest - ajout lot poids palpitations sommeil vertiges crampes

--- corpus-manifest.js
+++ corpus-manifest.js
@@ -50,5 +50,6 @@ window.MACA_CORPUS_MANIFEST = Object.freeze([
   'backlog-audited-harcelement-scolaire-2026-08-26.js?v=20260826-1',
   'backlog-audited-lot-eczema-psoriasis-spm-sii-2026-08-26.js?v=20260826-1',
   'backlog-audited-lot2-thyroide-fer-foie-naevus-paresthesies-lactose-2026-08-26.js?v=20260826-1',
-  'backlog-audited-ist-fatigue-taches-brunes-burnout-2026-08-26.js?v=20260826-1'
+  'backlog-audited-ist-fatigue-taches-brunes-burnout-2026-08-26.js?v=20260826-1',
+  'backlog-audited-lot-poids-palpitations-sommeil-vertiges-crampes-2026-08-26.js?v=20260826-1'
 ]);
