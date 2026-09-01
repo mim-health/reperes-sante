bf235b83dbab80264a218a2266cb9835d6e56607
mim-health <dr.beddok@gmail.com>
2026-09-01 16:54:14 +0200

    feat: brancher le lot 4 au corpus canonique

--- corpus-manifest.js
+++ corpus-manifest.js
@@ -60,5 +60,6 @@ window.MACA_CORPUS_MANIFEST = Object.freeze([
   'backlog-audited-prevention-urologie-senior-femme-2026-09-01.js?v=20260901-1',
   'backlog-audited-urologie-sexologie-femme-senior-lot2-2026-09-01.js?v=20260901-1',
   'backlog-audited-femme-senior-ophtalmo-douleurs-lot3-2026-09-01.js?v=20260901-1',
+  'backlog-audited-orl-ophtalmo-articulations-lot4-2026-09-01.js?v=20260901-1',
   'structured-backlog-compat.js?v=20260831-1'
 ]);
