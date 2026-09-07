6fde770817be2bbcaea515fcfc9d0028af211c8a
mim-health <dr.beddok@gmail.com>
2026-09-07 12:25:31 +0200

    feat(cancer): charger la migration editoriale validee

--- corpus-manifest.js
+++ corpus-manifest.js
@@ -66,5 +66,6 @@ window.MACA_CORPUS_MANIFEST = Object.freeze([
   'backlog-audited-editorial-rattrapage-2026-08-26-2026-09-04.js?v=20260905-1',
   'backlog-audited-creatine-sport-2026-09-05.js?v=20260905-1',
   'backlog-audited-femmes-ados-quotidien-2026-09-06.js?v=20260906-1',
+  'migration-cancer-editorial-2026-09-07.js?v=20260907-1',
   'structured-backlog-compat.js?v=20260831-1'
 ]);
